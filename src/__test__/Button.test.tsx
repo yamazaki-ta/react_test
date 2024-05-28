621dedbf145bec489b2331963dd60d07483cd5cb
yamazaki takashi <>
2024-05-28 17:56:27 +0900

    0528テストが動くまで記載

---
+++ src/__test__/Button.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Button from "../components/Button";
+
+describe("Button", () => {
+  it("buttonタグがレンダリングされる", () => {
+    render(<Button label="ボタン" onClick={() => alert("クリック")} />);
+    const element = screen.getByRole("button");
+    expect(element).toBeInTheDocument(); // buttonタグがレンダリングされる
+    expect(element).toHaveTextContent("ボタン"); //ラベルが表示されているか
+  });
+});
+
